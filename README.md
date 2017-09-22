635d9bc888723ccdeaea8af30d40873d0ba6c6e2
Tony Ivanov <telamohn@gmail.com>
2017-09-22 16:18:28 +0200

    First proof of concept complete

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Docker-rails base image
 
 
-A baseimage that supports development through docker
-hopefully without negative impact on production containers
+A minimalistic baseimage that supports development through docker hopefully
+without negative impact on production containers
 
 
 `project_template/` contains files that should exist in a project that uses this image.
@@ -14,3 +14,18 @@ Usage:
 
     # drop into the project's bash where ruby is available
     docker-compose run --rm dev bash
+
+### Using build-dependencies
+
+In order to keep your final image small, it's recommended to separate your
+dependencies into two categories `runtime` and `build`. 
+
+Use Virtual targets to add and purge packages within the same run-command.
+
+    RUN apk add --no-cache --virtual .build-deps \
+      list of dependencies \
+      && ./build_command \
+      && apk del .build-deps
+
+
+
--- ship.sh
+++ ship.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+# ship.sh
+# This script is our poor replacement for a continous deployment system.
+#
+# It builds your project's Dockerfile and pushes it to specified registry providing the following tags:
+# * Current git revision, e.g. `1.0.0-133-g3ca213f`
+# * Current branch name, e.g. `production`
+#
+# Place this file in your project root (sidelong with Dockerfile) , chmod +x ship.sh
+# and update the NAME variable.
+# Happy shipping!
 
-docker build -t inserve/rails:4 .
-docker push inserve/rails:4
+NAME=rails
+
+DOCKERFILE=Dockerfile
+
+# Grab tag from source-project
+TAG=$(git describe --tag)
+BRANCHTAG=$(git rev-parse --abbrev-ref HEAD)
+
+docker build \
+  --label app_version=${TAG} \
+  -t ${NAME}:${TAG} \
+  -t ${NAME}:${BRANCHTAG} \
+  -f $DOCKERFILE . && \
+docker push ${NAME}:${TAG} && \
+docker push ${NAME}:${BRANCHTAG} && \
+echo "Version: ${TAG} successfully shipped"
